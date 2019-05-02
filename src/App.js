f69cb4b199345bf2149cf473670ace35c3dc9389
Moonkitten <lauragreer3@gmail.com>
2019-05-01 23:17:45 -0600

    added game img container and css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App cover-container d-flex w-100 p-3 mx-auto flex-column">
+    <div className="App cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header className="App-header masthead mb-auto">
         <div className="inner">
-          <h3 className="masthead-brand">Marvel Clicky Game</h3>
+          <div className="title text-center"><h3 className="masthead-brand">Marvel Clicky Game</h3></div>
           <nav className="nav nav-masthead justify-content-center">
             <a className="nav-link active" href="#">Home</a>
             <a className="nav-link" href="#">Reset</a>
@@ -17,10 +17,16 @@ function App() {
           </nav>
         </div>
       </header>
-
       <main className="inner cover" role="main">
         <h1 className="cover-heading">Marvel Clicky Game</h1>
-        <p className="lead">Click a picture below to begin. Keep clicking characters without clicking same picture twice to build your score</p>
+        <p className="lead">Click a picture below to begin. Keep clicking characters without clicking same picture twice to build your score.</p>
+
+        <div id="game_pictures" className="container">
+          <div role="img" aria-label="click item" className="click-item shake" style={{ backgroundImage:"url(/game_images/loki.png)"}}></div>
+          
+        </div>
+
+
       </main>
 
       <footer className="mastfood mt-auto">
