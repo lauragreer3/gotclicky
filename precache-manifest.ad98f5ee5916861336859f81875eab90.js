36d2aa24cf22bfaaf80256997443cefa6b7877b9
Moonkitten <lauragreer3@gmail.com>
2019-05-14 20:41:58 -0600

    Updates

---
+++ precache-manifest.ad98f5ee5916861336859f81875eab90.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2f3e57eaa5da3c75f1ece9db23bdd7",
+    "url": "/gotclicky/index.html"
+  },
+  {
+    "revision": "5712e5a701ba39a307a7",
+    "url": "/gotclicky/static/css/main.d8395641.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/gotclicky/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "5712e5a701ba39a307a7",
+    "url": "/gotclicky/static/js/main.37dbf875.chunk.js"
+  },
+  {
+    "revision": "3a8cbc0b425003eb79d9",
+    "url": "/gotclicky/static/js/runtime~main.8643016e.js"
+  },
+  {
+    "revision": "06aa1e8cf0a5abce7a4dccfdbba589d3",
+    "url": "/gotclicky/static/media/bp.06aa1e8c.png"
+  },
+  {
+    "revision": "79bb9e5d1de1b122f7059a3b1bfb2acb",
+    "url": "/gotclicky/static/media/bpsis.79bb9e5d.png"
+  },
+  {
+    "revision": "812c071652e289e581e5690ddaad4e3f",
+    "url": "/gotclicky/static/media/captain-america.812c0716.png"
+  },
+  {
+    "revision": "661155cab27a296ecbb1aa9aec21e61a",
+    "url": "/gotclicky/static/media/gamora.661155ca.png"
+  },
+  {
+    "revision": "3e999e0ead36fd2b7eea91778b0b14fe",
+    "url": "/gotclicky/static/media/hulk.3e999e0e.png"
+  },
+  {
+    "revision": "efe20e40e36304fb45dbcd15833ff954",
+    "url": "/gotclicky/static/media/ironman.efe20e40.png"
+  },
+  {
+    "revision": "273e0ccce65f8589a145943d8bb1afb4",
+    "url": "/gotclicky/static/media/ironmonger.273e0ccc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gotclicky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "31289234ab87698d819f0b6f59d9309c",
+    "url": "/gotclicky/static/media/loki.31289234.png"
+  },
+  {
+    "revision": "55b145a1b0f97bb9993c6028f401f88b",
+    "url": "/gotclicky/static/media/mandarin.55b145a1.png"
+  },
+  {
+    "revision": "0f75ac65a319bf91c7f2e93529accc97",
+    "url": "/gotclicky/static/media/scarjo.0f75ac65.png"
+  },
+  {
+    "revision": "a5b6f1dd3649ba3b673c9f4c470f0e17",
+    "url": "/gotclicky/static/media/thanos.a5b6f1dd.png"
+  },
+  {
+    "revision": "1cd4b2614d66b056e015ab1880e1eca3",
+    "url": "/gotclicky/static/media/thor.1cd4b261.png"
+  }
+]);
